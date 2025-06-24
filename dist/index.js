c1045cd0ee9a83e853ea787ee17d552df18346a9
anish877 <62011810+anish877@users.noreply.github.com>
2025-06-24 12:59:13 +0530

    bindActionCreators

--- dist/index.js
+++ dist/index.js
@@ -20,3 +20,24 @@ function createStore(reducer) {
         subscribe
     };
 }
+function combineReducers(reducers) {
+    return function (state = {}, action) {
+        let newState = {};
+        for (let key in reducers) {
+            const reducer = reducers[key];
+            const prevStateForKey = state[key];
+            newState[key] = reducer(prevStateForKey, action);
+        }
+        return newState;
+    };
+}
+function bindActionCreators(actionCreators, dispatch) {
+    let bound = {};
+    for (let key in actionCreators) {
+        const actionCreator = actionCreators[key];
+        bound[key] = function (...args) {
+            return dispatch(actionCreator(...args));
+        };
+    }
+    return bound;
+}
